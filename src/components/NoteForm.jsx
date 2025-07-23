b05e293eca6bc339e9c4215abae48280dfede35f
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2025-07-23 16:20:48 +0300

    Prop drilling solved delete functionality added

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import TextInput from './inputs/TextInput';
+import SelectInput from './inputs/SelectInput';
+import TextAreaInput from './inputs/TextAreaInput';
 
 const NoteForm = ({ notes, setNotes }) => {
   const [formData, setFormData] = useState({
@@ -52,60 +55,42 @@ const NoteForm = ({ notes, setNotes }) => {
       {/* Form */}
       {isFormVisible && (
         <form onSubmit={handleSubmit} className='mb-6'>
-          <div className='mb-4'>
-            <label htmlFor='title' className='block font-semibold'>
-              Title
-            </label>
-            <input
-              name='title'
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              value={formData.title}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className='mb-4'>
-            <label htmlFor='priority' className='block font-semibold'>
-              Priority
-            </label>
-            <select
-              name='priority'
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              value={formData.priority}
-              onChange={handleChange}>
-              <option value='High'>🔴 High</option>
-              <option value='Medium'>🟠 Medium</option>
-              <option value='Low'>🟢 Low</option>
-            </select>
-          </div>
-          <div className='mb-4'>
-            <label htmlFor='category' className='block font-semibold'>
-              Category
-            </label>
-            <select
-              name='category'
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              value={formData.category}
-              onChange={handleChange}>
-              <option value='Work'>💼 Work</option>
-              <option value='Personal'>🏠 Personal</option>
-              <option value='Ideas'>💭 Ideas</option>
-            </select>
-          </div>
-          <div className='mb-4'>
-            <label htmlFor='description' className='block font-semibold'>
-              Description
-            </label>
-            <textarea
-              name='description'
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              value={formData.description}
-              onChange={handleChange}></textarea>
-          </div>
+          <TextInput
+            label='Title'
+            name='title'
+            value={formData.value}
+            onChange={handleChange}
+            required
+          />
+          <SelectInput
+            label='Priority'
+            name='priority'
+            value={formData.priority}
+            onChange={handleChange}
+            options={[
+              { label: '🔴 High', value: 'High' },
+              { label: '🟠 Medium', value: 'Medium' },
+              { label: '🟢 Low', value: 'Low' },
+            ]}
+          />
+          <SelectInput
+            label='Category'
+            name='category'
+            value={formData.category}
+            onChange={handleChange}
+            options={[
+              { label: '💼 Work', value: 'Work' },
+              { label: '🏠 Personal', value: 'Personal' },
+              { label: '💭 Ideas', value: 'Ideas' },
+            ]}
+          />
+          <TextAreaInput
+            label='Description'
+            name='description'
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
           <button className='w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600'>
             Add Note
           </button>
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,24 +1,13 @@
-const NoteList = ({ notes }) => {
+import Note from './Note';
+
+const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     return <p className='text-center text-gray-500'>No Notes Yet</p>;
   }
   return (
     <div className='space-y-4'>
       {notes.map((note) => (
-        <div
-          key={note.id}
-          className='bg-white p-4 rounded-lg shadow-md border-l-4'>
-          <h3 className='text-lg font-bold'>{note.title}</h3>
-          <p className='text-sm text-gray-600'>
-            <strong>Category:</strong>
-            {note.category}
-          </p>
-          <p className='text-sm text-gray-600'>
-            <strong>Priority:</strong>
-            {note.priority}
-          </p>
-          <p className='mt-2'>{note.description}</p>
-        </div>
+        <Note key={note.id} note={note} deleteNote={deleteNote} />
       ))}
     </div>
   );
